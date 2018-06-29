32101d1406a8d3034216dba169c25b07dbfca238
Duskhorizon <duskhorizon@home-desktop.home>
2018-06-29 19:42:14 +0200

    Done tutorial

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length = 200)
+    title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     image = models.ImageField(upload_to='images/')
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
