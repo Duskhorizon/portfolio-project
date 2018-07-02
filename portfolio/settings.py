0119646e03ea50669819c3b4aa654bce7aa14f58
Duskhorizon <duskhorizon@home-desktop.home>
2018-07-02 12:51:15 +0200

    Added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,3 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/image/'
+
+try:
+    from local_settngs import *
+except ImportError:
+    pass
+    
